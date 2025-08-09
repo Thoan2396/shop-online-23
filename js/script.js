4b91a7c8cef218f96e79025bcf65173badcf033c
Thoan2396 <Thoan2396@gmail.com>
2025-08-10 01:47:14 +0900

    Upload website

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+    });
+
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+    )
+    wow.init();
+});
